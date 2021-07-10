3d634c5814c5a529815ba11589088cd3f44c65c0
Golbinex <2061409-Golbinex@users.noreply.gitlab.com>
2021-07-10 22:10:53 +0200

    courbet: Switch to oss kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -23,25 +23,26 @@ TARGET_BOOTLOADER_BOARD_NAME := sm6150
 TARGET_NO_BOOTLOADER := true
 
 # Kernel
-BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom \
+BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 \
+			androidboot.hardware=qcom \
                         androidboot.memcg=1 \
                         lpm_levels.sleep_disabled=1 \
                         video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 \
                         service_locator.enable=1 \
                         swiotlb=1 \
-                        androidboot.usbcontroller=a600000.dwc3
+                        androidboot.usbcontroller=a600000.dwc3 \
+			earlycon=msm_geni_serial,0x880000
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
 BOARD_KERNEL_TAGS_OFFSET := 0x01E00000
 BOARD_RAMDISK_OFFSET     := 0x02000000
-BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
-BOARD_BOOTIMG_HEADER_VERSION := 2
-BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
-TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/Image
-TARGET_PREBUILT_DTB := $(DEVICE_PATH)/prebuilt/dtb
+BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
+BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_ARCH := arm64
-BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+TARGET_KERNEL_CONFIG := courbet_user_defconfig
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_SOURCE := kernel/xiaomi/courbet
 
 # Partitions
 BOARD_SUPER_PARTITION_SIZE := 9126805504
